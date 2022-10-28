fe9063179cd63568f5d978c625e79b9facfbf1dd
CoreySumma <CoreySumma@gmail.com>
2022-10-28 19:03:11 -0400

    Login/Out Buttons

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
+// This app has no "home" page, 
+// but your projects should 😀
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Collab Playlist' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optional
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    // Change to what's best for YOUR app
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    // Change path for your "landing" page
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 var methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,9 +27,22 @@ app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
