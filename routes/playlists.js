420cc362552f6abe230239c062272f7939466b79
CoreySumma <CoreySumma@gmail.com>
2022-11-02 13:08:18 -0400

    update

--- routes/playlists.js
+++ routes/playlists.js
@@ -10,7 +10,7 @@ router.get('/new', ensureLoggedIn, playlistsCtrl.new);
 // GET /playlists/:id (display show page for current songs/add song/add review buttons)
 router.get('/:id', ensureLoggedIn, playlistsCtrl.show)
 // POST /playlists (handles submission for a new playlist and posts it)
-router.post('/', ensureLoggedIn, playlistsCtrl.create);
+router.post('/', playlistsCtrl.create);
 // GET /playlists/:id/edit (edit playlist name)
 router.get('/:id/edit', ensureLoggedIn, playlistsCtrl.edit);
 // PUT /playlists/:id (update playlist name)
